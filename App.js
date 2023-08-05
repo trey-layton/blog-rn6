def3b7e808012a3480c2d921c6bbd54ae0d2f85e
Trey Layton <treylayton@Treys-Air.lan>
2023-08-05 08:02:21 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { Provider } from "./src/context/BlogContext";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
+import { Feather, EvilIcons } from "@expo/vector-icons";
+ 
+const Stack = createStackNavigator();
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerTitle: "Blogs" }}>
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                  <Feather name="plus" size={30} />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="Show"
+            component={ShowScreen}
+            options={({ route, navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("Edit", { id: route.params.id })
+                  }
+                >
+                  <EvilIcons name="pencil" size={35} />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Edit" component={EditScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://95a5-2603-8080-d340-b6-445d-491b-55fb-1145.ngrok-free.app'
+})
